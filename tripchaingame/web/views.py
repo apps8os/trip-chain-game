846dae1a5991624471a7292f7667966404f82efb
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-03-19 23:43:12 +0200

    Added view for last 24h trips.

--- tripchaingame/web/views.py
+++ tripchaingame/web/views.py
@@ -11,6 +11,8 @@ import json
 
 from ..models import Trip
 
+from ..util import gis
+
 from tripchaingame.models import Trip
 
 from mongoengine.django.auth import User
@@ -36,13 +38,26 @@ def view_trips(request):
         context['usernameset'] = request.user.get_full_name()
         context['user_name'] = request.user.username
         context['trips'] = ""+json.dumps([t.trip for t in Trip.objects.filter(user_id=uid)])
+
+        trips = [t.trip for t in Trip.objects.filter(user_id=uid)]
+        context['trips'] = json.dumps(trips)
     else:
-        context['trips'] = ""+json.dumps([t.trip for t in Trip.objects.all()])
+        context['trips'] = json.dumps([t.trip for t in Trip.objects.all()])
     #context['alltrips']=Trip.objects.all()
             #trips = ['']str(t.started_at) + " " + json.dumps(t.trip) + "<br>" for t in Trip.objects.all()]
-    
-    
+
     return render_to_response("view_routes.html", context, context_instance=RequestContext(request))
+
+@login_required
+def trips_today(request):
+    uid = _uid_from_user(request.user)
+    now = datetime.datetime.now()
+    yesterday = now - datetime.timedelta(days=1)
+
+    trips = [t.trip for t in Trip.objects.filter(user_id=uid, started_at__range=[yesterday, now])]
+    context = {'trips': json.dumps(trips)}
+
+    return render_to_response("view_routes.html", context)
 
 @login_required
 def my_trips(request):
