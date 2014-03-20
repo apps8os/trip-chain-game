c0e044a50ac5acb7595891bf71131e6f5fb9d44f
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-03-20 23:17:27 +0200

    Removed loading the data twice.

--- tripchaingame/web/views.py
+++ tripchaingame/web/views.py
@@ -29,23 +29,16 @@ def home(request):
 
 def view_trips(request):
     context = {}
-    context['trips'] = []
+
     context['plus_scope'] = ' '.join(settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE)
     context['plus_id'] = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
     
     if request.user.is_authenticated():
         uid = _uid_from_user(request.user)
-
-        context['usernameset'] = request.user.get_full_name()
-        context['user_name'] = request.user.username
-        context['trips'] = ""+json.dumps([t.trip for t in Trip.objects.filter(user_id=uid)])
-
         trips = [t.trip for t in Trip.objects.filter(user_id=uid)]
         context['trips'] = json.dumps(trips)
     else:
         context['trips'] = json.dumps([t.trip for t in Trip.objects.all()])
-    #context['alltrips']=Trip.objects.all()
-            #trips = ['']str(t.started_at) + " " + json.dumps(t.trip) + "<br>" for t in Trip.objects.all()]
 
     return render_to_response("view_routes.html", context, context_instance=RequestContext(request))
 
