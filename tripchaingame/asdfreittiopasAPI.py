7359d1d55df993d3f1748a3baad874608978d9ae
claire <minna.tamper@aalto.fi>
2014-04-13 10:51:57 +0300

    Adding missing files

---
+++ tripchaingame/asdfreittiopasAPI.py
@@ -0,0 +1,34 @@
+import logging
+
+#Logging
+logger = logging.getLogger(__name__)
+
+class ReittiopasAPI:
+    def __init__(self):
+        #init
+        self.__epsg_in='wgs84'
+        self.__epsg_out='wgs84'
+        self.__user='tripchaingame'
+        self.__passwd='nuy0Fuqu'
+        
+    def get_reverse_geocode(coordinates):
+        #http://api.reittiopas.fi/hsl/prod/?request=reverse_geocode&coordinate=24.8829521,60.1995236&epsg_in=wgs84&epsg_out=wgs84&user=tripchaingame&pass=nuy0Fuqu
+        result = ""
+        parameters = {'request': 'reverse_geocode', 
+                      'coordinate': coordinates, 
+                      'epsg_in':self.__epsg_in, 
+                      'epsg_out':self.__epsg_out,
+                      'user':self.__user,
+                      'pass': self.__passwd}
+        json_response = requests.get("http://api.reittiopas.fi/hsl/prod/", params=parameters)
+        if json_response.status_code == requests.codes.ok:
+            r = json.dumps(json_response.json())
+            routes = json.loads(r)
+            for route in routes:
+                r = json.dumps(route["name"])
+                result = r.replace('"',"")
+        else:
+            logger.warn(json_response.status_code)
+            json_response.raise_for_status()
+            
+        return result
---
+++ tripchaingame/web/point.py
@@ -0,0 +1,71 @@
+import logging
+import json
+
+'''
+    A class dedicated to work on place recognition logic.
+'''
+#Logging
+logger = logging.getLogger(__name__)
+
+class Point:
+    def __init__(self):
+        #init vars
+        self._coords = []
+        self._address = ""
+        self.__count_of_points = 0
+        self.__threshold_value = 0
+        
+    def set_threshold_value(self, value):
+        self.__threshold_value = value
+        
+    def get_threshold_value(self, value):
+        return self.__threshold_value 
+        
+    def get_points(self):
+        return self.__count_of_points
+    
+    def set_points(self, count):
+        self.__count_of_points = count
+        
+    def add_point(self):
+        self.__count_of_points = self.__count_of_points+1
+    
+    def get_coords(self):
+        return self._coords
+    
+    def pop_coords(self):
+        if len(self._coords) == 1:
+            return self._coords.pop()
+        else:
+            return 0
+    
+    def set_coords(self, coordinates):
+        if coordinates not in self._coords:
+            logger.warn("Point (%s) %s not found from coordinates table" % (self.get_address(),coordinates))
+            self._coords.append(coordinates)
+        else:
+            logger.warn("Point %s found from coordinates table" % coordinates)
+    
+    def get_address(self):
+        return self._address
+    
+    def set_address(self, addr):
+        self._address = addr
+        
+    def __len__(self):
+        return len(self._address)
+    
+    def __eq__(self, other):
+        return self.get_address() == other.get_address()
+    
+    def __str__(self):
+        str = "%s (%s), %s\n" % (self.get_address(), self.get_coords(), self.get_points())
+        return str
+    
+    def find_element(self, element):
+        try:
+            index_element=self._coords.index(element)
+            return index_element
+        except ValueError:
+            return -1
+    
